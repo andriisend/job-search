3d710ba3f25e70cbf822596efd8ae90dca042cc2
andriisend <111528254+andriisend@users.noreply.github.com>
2022-12-08 19:39:19 -0500

    Job Search Initial Implement

--- app/jobsearch1.py
+++ app/jobsearch1.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from dataclasses import dataclass
-import requests
-import json 
-from requests.auth import HTTPBasicAuth
-from alpha import api_key
-from alpha import user_agent
+from models import job_criterias, get_searchterms
+from api import fetch_jobs_data
+from alpha import api_key, user_agent
 
-def result(n):
+if 'criterias' not in st.session_state:
+    st.session_state.criterias = []
+
+def print_result(n):
     st.write(('----------------'))
     st.write(str((n['MatchedObjectDescriptor']['PositionTitle'])))
     st.write(str(n['MatchedObjectDescriptor']['PositionLocation'][0]['LocationName']))
@@ -16,117 +16,20 @@ def result(n):
   # if float(n['MatchedObjectDescriptor']['PositionRemuneration'][0]['MinimumRange']) >= float(criteria):
     st.write('Salary: ' + str(n['MatchedObjectDescriptor']['PositionRemuneration'][0]['MinimumRange']) + ' - ' +
                               str(n['MatchedObjectDescriptor']['PositionRemuneration'][0]['MaximumRange']))
-def results(n):
-  for n in data['SearchResult']['SearchResultItems']:
-    st.write(result(n))
 
-def fetch_jobs_data():
-    my_headers = {'Host': 'data.usajobs.gov', 'User-Agent': str(user_agent),'Authorization-Key': str(api_key)}
-    response = requests.get(f'https://data.usajobs.gov/api/search?{search_by}={criteria}', headers=my_headers)
-    #print(f'https://data.usajobs.gov/api/search?{search_by}={criteria}')
-    data = json.loads(response.text)
-    for n in data['SearchResult']['SearchResultItems']:
-        result(n)
+st.markdown("# Job Search")
 
+with st.sidebar:
+  for criteria in job_criterias:
+    if criteria.sidebar:
+      st.session_state[criteria.value] = st.text_input(criteria.label)
 
-@dataclass
-class Criteria:
-  label: str
-  value: str
+for criteria in job_criterias:
+  if criteria.sidebar != True:
+    st.session_state[criteria.value] = st.text_input(criteria.label)
 
-
-def fetch_code_list(criteria):
-  request_url = f"https://data.usajobs.gov/api/codelist/{dictionary_criteria.value}"
-  response = requests.get(request_url)
-  data = json.loads(response.text)
-  return data
-
-position_schedules = [
-  None,
-  Criteria('Full-Time', '1'),
-  Criteria('Part-Time', '2'),
-  Criteria('Shift-Work', '3'),
-  Criteria('Intermittent', '4'),
-  Criteria('Job Sharing', '5'),
-  Criteria('Multiple Schedules', '6'),
-]
-
-travel_requirements = [
-    None,
-    Criteria('Not Required', '0'),
-    Criteria('Occasional Travel', '1'),
-    Criteria('25% or Less', '2'),
-    Criteria('50% or less', '5'),
-    Criteria('75% or less', '7'),
-    Criteria('76% or greater', '8')
-]
-
-job_criteria = [
-    None,
-    Criteria('Position Schedule Type', 'PositionScheduleTypeCode'),
-    Criteria('Minimum Salary', 'RemunerationMinimumAmount'),
-    Criteria('Keyword', 'Keyword'),
-    Criteria('Position Title', 'PositionTitle'),
-    Criteria('Job Category Code', 'JobCategoryCode'),
-]
-
-dictionary_criteria = [
-  None,
-  Criteria('Agency Subelements', 'agencysubelements'),
-  Criteria('Occupational Series', 'occupationalseries'),
-  Criteria('Pay Plans', 'payplans'),
-  Criteria('Postal Codes', 'postalcodes')
-]
-
-def get_label(criteria):
-  if criteria == None:
-    return "-"
-  return criteria.label
-
-st.write("Job Search")
-
-codeList = st.selectbox(
-    'Code List',
-    dictionary_criteria,
-    index=0,
-    format_func=get_label
-)
-
-position_schedule = st.selectbox(
-    'Position Schedule',
-    position_schedules,
-    index=0,
-    format_func=get_label
-)
-
-travel_requirement = st.selectbox(
-    'Travel Preferences',
-    travel_requirements,
-    index=0,
-    format_func=get_label
-)
-
-keyword = st.text_input('Position Title')
-
-minimum_salary = st.text_input('Minimum Salary')
-#keyword_search = 'Keyword=' + keyword
-#position_schedule_search = 'PositionScheduleTypeCode' + position_schedule
-
-
-def search_unit():
-    return 'Keyword=' + keyword
-    if position_schedule != None:
-        return 'PositionScheduleType=' + position_schedule
-
-
-
-# if option != None and option1 != None and option2 != None:
-#   st.write(fetch_code_list(option))
-if st.button('Search', disabled=keyword==""):
-  searches = []
-  if codeList != None:
-    searches.append(codeList.value)
-  if position_schedule != None:
-    searches.append(position_schedule.value)
-  searches.append(keyword)
-  st.write(searches)
+if st.button("Search"):
+  results = fetch_jobs_data(get_searchterms(st.session_state), api_key, user_agent)
+  # whatever
+  for n in results:
+    print_result(n)
---
+++ app/models.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+
+@dataclass
+class Criteria:
+  label: str
+  value: str
+  sidebar: bool = True
+
+  def __eq__(self, other):
+    """Overrides the default implementation"""
+    if str(type(other)) == str(type(self)):
+      return self.value == other.value
+    return False
+
+def get_label_from_criteria(criteria):
+  if criteria == None:
+    return "-"
+  return criteria.label
+
+@dataclass
+class SearchInput:
+  criteria: Criteria
+  value: str
+
+  def __eq__(self, other):
+    """Overrides the default implementation"""
+    if str(type(other)) == str(type(self)):
+      return self.value == other.value and str(type(other.criteria)) == str(type(self.criteria))
+    return False
+
+def get_searchterms(search_input_storage):
+  search_terms = []
+  for criteria in job_criterias:
+    if search_input_storage[criteria.value] != "":
+      search_terms.append(f"{criteria.value}={search_input_storage[criteria.value]}")
+  return search_terms
+
+position_schedules = [
+  None,
+  Criteria('Full-Time', '1'),
+  Criteria('Part-Time', '2'),
+  Criteria('Shift-Work', '3'),
+  Criteria('Intermittent', '4'),
+  Criteria('Job Sharing', '5'),
+  Criteria('Multiple Schedules', '6'),
+]
+
+travel_requirements = [
+    None,
+    Criteria('Not Required', '0'),
+    Criteria('Occasional Travel', '1'),
+    Criteria('25% or Less', '2'),
+    Criteria('50% or less', '5'),
+    Criteria('75% or less', '7'),
+    Criteria('76% or greater', '8')
+]
+
+job_criterias = [
+    Criteria('Position Schedule Type', 'PositionScheduleTypeCode'),
+    Criteria('Minimum Salary', 'RemunerationMinimumAmount'),
+    Criteria('Keyword', 'Keyword', False),
+    Criteria('Position Title', 'PositionTitle'),
+    Criteria('Job Category Code', 'JobCategoryCode'),
+]
+
+dictionary_criteria = [
+  None,
+  Criteria('Agency Subelements', 'agencysubelements'),
+  Criteria('Occupational Series', 'occupationalseries'),
+  Criteria('Pay Plans', 'payplans'),
+  Criteria('Postal Codes', 'postalcodes')
+]
